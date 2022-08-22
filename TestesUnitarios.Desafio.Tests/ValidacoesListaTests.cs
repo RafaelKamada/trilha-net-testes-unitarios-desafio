c9bf3da914cdd38d4e6371e6c2647cb1fa93ab3d
RKamada <rafael-kamada2007@hotmail.com>
2022-08-22 18:47:56 -0300

    Desafio concluido

--- TestesUnitarios.Desafio.Tests/ValidacoesListaTests.cs
+++ TestesUnitarios.Desafio.Tests/ValidacoesListaTests.cs
@@ -44,8 +44,10 @@ public class ValidacoesListaTests
         var numeroParaProcurar = 10;
 
         // Act
+        var resultado = _validacoes.ListaContemDeterminadoNumero(lista, numeroParaProcurar);
 
         // Assert
+        Assert.False(resultado);
     }
 
     //TODO: Corrigir a anotação [Fact]
@@ -54,12 +56,15 @@ public class ValidacoesListaTests
         //TODO: Implementar método de teste
 
         // Arrange
+        int multiplicarPor = 2;
         var lista = new List<int> { 5, 7, 8, 9 };
         var resultadoEsperado = new List<int> { 10, 14, 16, 18 };
         
         // Act
+        var resultado = _validacoes.MultiplicarNumerosLista(lista, multiplicarPor);
 
         // Assert
+        Assert.Equal(resultadoEsperado, resultado);
     }
 
     [Fact]
@@ -68,13 +73,15 @@ public class ValidacoesListaTests
         //TODO: Implementar método de teste
 
         // Arrange
+        int retornoEsperado = 9;
         var lista = new List<int> { 5, -1, -8, 9 };
 
         // Act
+        var resultado = _validacoes.RetornarMaiorNumeroLista(lista);
 
         // Assert
         //TODO: Corrigir o Assert.Equal com base no retorno da chamada ao método
-        Assert.Equal(9, 9);
+        Assert.Equal(retornoEsperado, resultado);
     }
 
     [Fact]
@@ -83,6 +90,7 @@ public class ValidacoesListaTests
         //TODO: Implementar método de teste
 
         // Arrange
+        int retornoEsperado = -8;
         var lista = new List<int> { 5, -1, -8, 9 };
 
         // Act
@@ -90,6 +98,6 @@ public class ValidacoesListaTests
 
         // Assert
         //TODO: Corrigir o Assert.Equal com base no retorno da chamada ao método
-        Assert.Equal(-8, -8);
+        Assert.Equal(retornoEsperado, resultado);
     }
 }
